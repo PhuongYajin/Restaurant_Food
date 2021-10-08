c11108d895d9f416111fc137e25d4ac1ca2ef9c5
phuongyajin_FPTU <phuongyajin@gmail.com>
2021-10-08 19:47:31 +0700

    home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from '../../components/Header';
+import Suggest from './home-components/Home';
+import Speciality from './home-components/Speciality';
+import Popular from './home-components/Popular';
+import Gallery from './home-components/Gallery';
+import Reviews from './home-components/Reviews';
+import Order from './home-components/Order';
+import Footer from '../../components/Footer';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Suggest />
+      <Speciality />
+      <Popular />
+      <Gallery />
+      <Reviews />
+      <Order />
+      <Footer />
+    </div>
+  );
+}
