3aba395f111500238420d6b5d2c45b9e8864214a
phuongyajin_FPTU <phuongyajin@gmail.com>
2021-10-08 19:39:08 +0700

    config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'jsx-a11y',
+  ],
+  rules: {
+    'react/no-unescaped-entities': [
+      'error',
+      {
+        forbid: [{ char: '>', alternatives: ['&gt;'] }, { char: '}', alternatives: ['&#125;'] }],
+      },
+    ],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'import/prefer-default-export': 'off',
+  },
+};
