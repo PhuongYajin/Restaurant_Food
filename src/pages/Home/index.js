0101e478c50ba2704f8790b1b166619e72a0d6de
phuongyajin_FPTU <phuongyajin@gmail.com>
2021-11-16 17:14:19 +0700

    change tag div

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,7 +10,7 @@ import Footer from '../../components/Footer';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
       <Suggest />
       <Speciality />
@@ -19,6 +19,6 @@ export default function Home() {
       <Reviews />
       <Order />
       <Footer />
-    </div>
+    </>
   );
 }
